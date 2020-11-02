d83d6a9e6e2ae81c7f4b8b25ce2ec0e0f684c707
prajaktaaD <70463406+prajaktaaD@users.noreply.github.com>
2020-11-02 15:46:13 +0530

    Create Basic7.c
    7.Accept two numbers and display quotient and a reminder of division::

---
+++ Basic7.c
@@ -0,0 +1,14 @@
+//7.Accept two numbers and display quotient and reminder of division::
+
+#include<stdio.h>
+int main()
+{
+  int num1,num2;
+  printf("Enter the two numbers :");
+  scanf("%d%d",&num1,&num2);
+  printf("\nQuotient=%d",num1/num2);
+  printf("\nReminder=%d",num1%num2);
+  
+  return 0;
+  
+ } 
