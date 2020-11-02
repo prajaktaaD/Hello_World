2a1d16cf5b6f7ff987380962422a6de06983ba2e
prajaktaaD <70463406+prajaktaaD@users.noreply.github.com>
2020-11-02 15:13:00 +0530

    Create Basic2.c
    2.Accept two numbers and display their addition.

---
+++ Basic2.c
@@ -0,0 +1,13 @@
+//2.Accept two numbers and display their addition:
+
+
+#include<stdio.h>
+int main()
+{
+   int a,b;
+   printf("Enter two num:");
+   scanf("%d %d",&a,&b);
+   printf("Sum of two numbers=%d",a+b);
+   
+   return0;
+ }  
