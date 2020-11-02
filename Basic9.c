e890e8a7dbcb50f64e927b535768c588b2b64be1
prajaktaaD <70463406+prajaktaaD@users.noreply.github.com>
2020-11-02 15:56:08 +0530

    Create Basic9.c

---
+++ Basic9.c
@@ -0,0 +1,20 @@
+//9.Display sizeof variable using size of oprator:
+
+#include<stdio.h>
+int main()
+{
+  int Int_var;
+  char Char_var;
+  float Float_var;
+  double Double_var;
+  
+  
+  printf("size of int:%d",sizeof(Int_var));
+  printf("\nsize of char:%d",sizeof(Char_var));
+  printf("\nsize of float:%d",sizeof(Float_var));
+  printf("\nsize of double:%d",sizeof(Double_var));
+  
+  return 0;
+  
+ } 
+  
